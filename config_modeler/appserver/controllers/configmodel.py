893f8b2c85445cbb93a6d28f54fed4812fcd3bab
bernie macias <berniem@zillow.com>
2015-09-30 15:52:03 -0700

    added splunk imports

---
+++ config_modeler/appserver/controllers/configmodel.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+# Author: Bernardo Macias <bmacias@httpstergeek.com>
+#
+#
+# All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'Bernardo Macias '
+__credits__ = ['Bernardo Macias']
+__license__ = "ASF"
+__version__ = "2.0"
+__maintainer__ = "Bernardo Macias"
+__email__ = 'bmacias@httpstergeek.com'
+__status__ = 'Production'
+
+import os
+import cherrypy
+import logging
+import logging.handlers
+import json
+import sys
+import re
+
+# SPLUNK IMPORTS
+import splunk, splunk.util
+import splunk.appserver.mrsparkle.controllers as controllers
+from splunk.appserver.mrsparkle.lib.decorators import expose_page
+from splunk.appserver.mrsparkle.lib.routes import route
+from splunk.appserver.mrsparkle.lib import jsonresponse, util, cached
