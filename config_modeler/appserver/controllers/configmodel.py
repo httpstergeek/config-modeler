d27bdabdfaab67c048387db6fbcc0a5cae47baba
bernie macias <berniem@zillow.com>
2015-10-01 13:42:16 -0700

    Add method dectection.\n get method returns list of apps

--- config_modeler/appserver/controllers/configmodel.py
+++ config_modeler/appserver/controllers/configmodel.py
@@ -41,11 +41,14 @@ from splunk.appserver.mrsparkle.lib import jsonresponse, util, cached
 
 
 def setup_logger(level):
-    logger = logging.getLogger('config_modeler')
-    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    appname = os.path.dirname(os.path.realpath(__file__)).split('/')[-2]
+    logger = logging.getLogger(appname)
+    logger.propagate = False  # Prevent the log messages from being duplicated in the python.log file
     logger.setLevel(level)
 
-    file_handler = logging.handlers.RotatingFileHandler(os.path.join(os.environ.get("SPLUNK_HOME"), 'var', 'log', 'splunk', 'config_modeler.log'), maxBytes=25000000, backupCount=5)
+    file_handler = logging.handlers.RotatingFileHandler(
+        os.path.join(os.environ.get("SPLUNK_HOME"), 'var', 'log', 'splunk', appname + '.log'), maxBytes=25000000,
+        backupCount=5)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     file_handler.setFormatter(formatter)
 
@@ -53,7 +56,19 @@ def setup_logger(level):
 
     return logger
 
-logger = setup_logger(logging.INFO)
 
-class TerminalController(controllers.BaseController):
-  pass
+logger = setup_logger(logging.INFO)
+deploymentapps = os.path.join(os.environ['SPLUNK_HOME'], 'etc', 'deployment-apps')
+
+
+class ConfigModelerController(controllers.BaseController):
+    @expose_page(must_login=False, methods=['GET', 'POST'])
+    @route('/', methods=['GET', 'POST'])
+    def configmodel(self, **kwargs):
+        method = cherrypy.request.method
+
+        if method == 'GET':
+            return json.dumps(next(os.walk(deploymentapps))[1])
+
+        if method == 'POST':
+            return kwargs.get('data')
